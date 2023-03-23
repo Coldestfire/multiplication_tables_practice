a958423f472a1744906dfe969f06a9960e0c10b1
Coldestfire <41896570+Coldestfire@users.noreply.github.com>
2023-03-23 18:53:33 +0530

    Add files via upload

---
+++ Multiplication Tables Practice/Multiplication Tables Practice.py
@@ -0,0 +1,35 @@
+x = input("Tables of Which Number: ")
+print(" ")
+
+
+
+i=1
+correct_answers = 0
+wrong_answers = 0
+
+
+
+while i<11:
+    print(str(x) + " X " + str(i) + " = ")
+
+    answers = int(input())
+
+    if answers == int(i)*int(x):
+        print("Correct")
+        print("-------------------")
+        correct_answers += 1
+    else:
+        print("Wrong")
+        print("Answer is " + str(int(i)*int(x)))
+        print("-------------------")
+        wrong_answers += 1
+
+    i = i + 1
+
+
+#Results
+if correct_answers == 10:
+    print("Awesome!!! You got all of them Correct!!!")
+
+else:
+    print("You got " + str(correct_answers) + " Correct and " + str(wrong_answers) + " Wrong")
